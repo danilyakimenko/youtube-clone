0d8bf378a7d9c799d113c8d19c22279f950c0e55
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-22 14:59:33 +0700

    #6 youtube-clone: -Metadata для страниц

--- src/app/[categoryId]/page.tsx
+++ src/app/[categoryId]/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Videos in category: ...",
+};
+
 type CategoryPageProps = {
   params: Promise<{ categoryId: string }>
 }
--- src/app/profile/[profileId]/page.tsx
+++ src/app/profile/[profileId]/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Profile: ...",
+};
+
 type ProfilePageProps = {
   params: Promise<{ profileId: string }>
 }
