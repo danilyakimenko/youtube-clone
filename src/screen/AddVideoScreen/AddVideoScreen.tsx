4b17e40e0c949ce6db8e9f873f63a94e1d9a7798
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-22 16:04:46 +0700

    #8 youtube-clone: -Обработка формы с помощью react-hook-form и zod

--- src/screen/AddVideoScreen/AddVideoScreen.tsx
+++ src/screen/AddVideoScreen/AddVideoScreen.tsx
@@ -1,43 +1,72 @@
 'use client'
 
 import { useState } from 'react'
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 import parseYouTube from '@/shared/libs'
 import styles from './AddVideoScreen.module.css'
+
+const schema = z.object({
+  videoUrl: z.string().min(1, { message: 'The field must not be empty.' }),
+})
+
+type Inputs = {
+  videoUrl: string
+}
 
 const AddVideoScreen = () => {
   const [videoId, setVideoId] = useState('')
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: zodResolver(schema),
+  })
+
+  const onSubmit = (data: Inputs) => {
+
+    const url = data.videoUrl
+
+    if (!url) return
+
+    let finalUrl: URL | null = null
+
+    try {
+      finalUrl = new URL(url)
+    } catch (error) {
+      console.error('error', error)
+    }
+
+    if (!finalUrl) return
+
+    const videoId = parseYouTube(finalUrl)
+
+    if (!videoId) return
+
+    setVideoId(videoId)
+  }
+
+  const videoUrlErrorMessage = errors.videoUrl?.message
+  const hasVideoUrlInputError = Boolean(videoUrlErrorMessage)
+
   return (
     <div>
       <form
-        onSubmit={(event) => {
-          event.preventDefault()
-          const anyEvent = event as any
-          const url = anyEvent.target.elements[0].value
-
-          if (!url) return
-
-          let finalUrl: URL | null = null
-
-          try {
-            finalUrl = new URL(url)
-          } catch (error) {
-            console.error('error', error)
-          }
-
-          if (!finalUrl) return
-
-          const videoId = parseYouTube(finalUrl)
-
-          if (!videoId) return
-
-          setVideoId(videoId)
-        }}
+        onSubmit={handleSubmit(onSubmit)}
       >
-        <input
-          type="text"
-          placeholder="Insert the link to the video"
-        />
+        <label>
+          <input
+            type="text"
+            placeholder="Insert the link to the video"
+            {...register(`videoUrl`)}
+          />
+          {hasVideoUrlInputError && (
+            <p>{videoUrlErrorMessage}</p>
+          )}
+        </label>
         <button
           type="submit"
         >
